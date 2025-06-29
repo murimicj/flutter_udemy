417493e46513752d9450d20c0361e218c5280c8d
Charles Murimi <mjohnson@kabarak.ac.ke>
2025-06-29 17:19:15 +0300

    States

---
+++ practice_app/lib/data/notifiers.dart
@@ -0,0 +1,7 @@
+//Value notifier
+//Valuelistenablebuilder: listen to data(dont need set state)
+
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
+ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(true);
--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -1,44 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:practice_app/data/notifiers.dart';
+import 'package:practice_app/views/widget_tree.dart';
+// ignore: unused_import
+import 'package:practice_app/views/widgets/navbar_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
-//stateless
+//stateless-cant refresh
 //!Material App
 //?Scafold
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState()=>_MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{  
+ 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier, 
+      builder: (context,isDarkMode,child){
+       return  MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
-          brightness: Brightness.dark,
+          brightness: isDarkMode?Brightness.dark:Brightness.light,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Map'),
-          centerTitle: true,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-             NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          
-        ),
-      ),
+      home:WidgetTree(),
     );
+      },
+      );
   }
 }
